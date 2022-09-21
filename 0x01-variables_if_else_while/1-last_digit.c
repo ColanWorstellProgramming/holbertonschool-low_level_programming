18a11f2ad655fa513c6633c8eba491c1cb7c6061
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-21 11:37:27 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,36 +16,30 @@ srand(time(0));
 
 n = rand() - RAND_MAX / 2;
 
-char nx[] = "Last digit of ";
-
-char nx1[] = strcat(nx, n);
-char nx2[] = strcat(nx1, "is ");
-char nxf[];
+char nx[] = "Last digit of %d is %d ", n, n % 10;
 
 if (n > 5)
 
 {
 
-nxf[] = strcat(nx2, "and is greater than 5")
+printf(nx, "and is greater than 5");
 
 }
 else if (n == 0)
 {
 
-nxf[] = strcat(nx2, "and is 0")
+printf(nx, "and is 0");
 
 }
 
 else
 {
 
-nxf[] = (nx2, "and is less than 6 and not 0")
+printf(nx, "and is less than 6 and not 0");
 
 }
 
-char nxf2[] = (nfx, '\n')
-
-snprintf(nxf2);
+printf("\n");
 
 return (0);
 
