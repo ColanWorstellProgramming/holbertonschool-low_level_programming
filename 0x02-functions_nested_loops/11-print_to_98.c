6c9cd5d18bd12ab2aa193b5b7f198fc3557df02b
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-21 15:11:08 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,20 +15,24 @@ void print_to_98(int n)
 if (n <= 98)
 {
 
-while (n <= 98)
+while (n < 98)
 {
 printf("%d\n", n);
 n++;
 }
 
 }
+
 if (n >= 98)
 {
 
-while (n >= 98)
+while (n > 98)
 {
 printf("%d\n", n);
 n--;
+
 }
 }
+
+
 }
