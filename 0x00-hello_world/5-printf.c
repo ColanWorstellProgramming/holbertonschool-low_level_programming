cc327d3fb3ecbbfb02fe14f887e1b1f14a04fce2
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-20 21:51:54 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - starts file
+ *
+ * Return: ends file
+ *
+ */
+
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+
+}
