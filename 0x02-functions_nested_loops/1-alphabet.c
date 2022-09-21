aaa5d51f36892e9d9d73f2f7793d0e43e4018637
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-21 12:19:56 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * main - | starts file
+ * Return: - | ends file
+ */
+
+void print_alphabet(void)
+{
+
+char x;
+
+for ( x = 'a'; x <= 'z'; x++)
+{
+
+_putchar(x);
+
+}
+
+_putchar('\n');
+
+}
