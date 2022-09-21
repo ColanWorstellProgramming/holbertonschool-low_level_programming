2e5a525ba4b56ed8a0ce1410aa7740428937b721
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-20 21:44:40 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+/*
+ * 4
+ */
 
-int main(void){
+int main(void)
+{
 
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 
 }
