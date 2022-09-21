3b3b7e5400ed92560c55fbf671ee0c59ef7a2f80
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-21 13:11:37 -0700

    8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,7 +11,7 @@ int main(void)
 int i;
 char x;
 
-for (i = 0; i <= 10; i++)
+for (i = 0; i < 10; i++)
 {
 
 putchar((i % 10) + '0');
@@ -24,6 +24,8 @@ for (x = 'a'; x <= 'f'; x++)
 putchar(x);
 
 }
+
+putchar('\n');
 
 return (0);
 
