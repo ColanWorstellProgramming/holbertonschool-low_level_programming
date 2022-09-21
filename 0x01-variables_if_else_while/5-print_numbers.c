1350a16a1b62595d9769c1c423636c7ac8ba1875
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-21 12:10:20 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - | starts file
+ * Return: - | ends file
+ */
+
+int main(void)
+{
+
+int i;
+
+for (i = '0'; i <= '10'; i++)
+{
+
+printf(i);
+
+}
+
+printf('\n');
+
+return (0);
+
+}
