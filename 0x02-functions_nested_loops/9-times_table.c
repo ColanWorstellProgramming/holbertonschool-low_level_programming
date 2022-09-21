89da030dff80e46861f2d651b7fa9d45087fd3d4
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-21 14:15:16 -0700

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * times_table - | starts file
+ * Return: - | ends file
+ */
+
+void times_table(void)
+{
+
+int x, y;
+
+for (x = 0; x < 10; x++)
+{
+_putchar('0,  ');
+}
+
+for (y = 0; y < 10; y++)
+{
+printf("%d,  ",y*1);
+}
+
+}
