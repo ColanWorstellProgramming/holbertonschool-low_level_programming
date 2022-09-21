f55c33516a384ea48c6406fb87b5120e2a258562
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-21 12:46:58 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,10 +14,10 @@ void print_alphabet_x10(void)
 char x;
 int y;
 
-for ( y = 0; y < 10; y++)
+for (y = 0; y < 10; y++)
 {
 
-for ( x = 'a'; x <= 'z'; x++)
+for (x = 'a'; x <= 'z'; x++)
 {
 
 _putchar(x);
