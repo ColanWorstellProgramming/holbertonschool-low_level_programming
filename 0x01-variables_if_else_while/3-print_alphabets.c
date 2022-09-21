6d12ce7410d2da3212ca08be7bd3c237640b343f
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-21 12:07:48 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - | starts file
+ * Return: - | ends file
+ */
+
+int main(void)
+{
+
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+
+putchar(i);
+
+}
+
+char y;
+
+for (y = 'A'; y <= 'Z'; y++)
+{
+
+putchar(y);
+
+}
+
+putchar('\n');
+
+return (0);
+
+}
