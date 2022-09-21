93ca1eca747bab7242d7aa607041a23a3508deb5
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-21 13:39:48 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * _abs - | starts file
+ * @n: - | int
+ * Return: - | ends file
+ */
+
+int _abs(int n)
+{
+
+if (n >= 0)
+{
+
+return (n);
+
+}
+
+else
+
+return (-n)
+
+}
