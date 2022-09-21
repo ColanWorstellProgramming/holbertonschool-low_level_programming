759005a4ba171d75cb61d2cd33dad136261ced08
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-21 14:00:46 -0700

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * jack_bauer - | starts file
+ * @n: - | int
+ * Return: - | ends file
+ */
+
+void jack_bauer(void)
+{
+
+int min, hour;
+
+for (min = 0; min < 60; min++)
+{
+
+for (hour = 0; hour < 60; hour++)
+{
+
+printf("%02d:%02d", hour, min);
+
+}
+
+}
+
+}
