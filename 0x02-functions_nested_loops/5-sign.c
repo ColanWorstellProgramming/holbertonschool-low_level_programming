eea4bf3061276d5c025d04f2b2c269a1ed2a4527
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-21 13:36:40 -0700

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,21 +13,21 @@ int print_sign(int n)
 {
 
 if (n > 0)
-
-_putchar('+');
+{
+putchar('+');
 
 return (1);
-
+}
 else if (n == 0)
-
+{
 _putchar('0');
 
 return (0);
-
+}
 else
-
+{
 _putchar('-');
 
 return (-1);
-
+}
 }
