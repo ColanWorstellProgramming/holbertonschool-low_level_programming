5cd2008d99ecdfc720ac47f35b282e12add46d3c
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-21 13:14:14 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * main - | starts file
+ *
+ * Return: - | ends file
+ */
+
+int main(void)
+{
+
+putchar(_putchar);
+
+putchar('\n');
+
+return (0);
+
+}
