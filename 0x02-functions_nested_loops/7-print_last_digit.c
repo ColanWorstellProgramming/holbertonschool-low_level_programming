d11634c4e3957d5867f194ead132811e527237ce
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-21 13:43:28 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * print_last_digit - | starts file
+ * @n: - | int
+ * Return: - | ends file
+ */
+
+int print_last_digit(int n)
+{
+
+int x = n % 10;
+
+return (x);
+
+}
