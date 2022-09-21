85fc380f963207955c7b1aa00b357c1ffb832f91
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-21 15:00:56 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * add - | starts file
+ * Return: - | ends file
+ */
+
+int add(add, add)
+{
+
+return (add + add);
+
+}
