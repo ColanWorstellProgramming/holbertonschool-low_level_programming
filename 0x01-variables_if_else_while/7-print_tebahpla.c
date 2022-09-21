f68494524983abd0885ba91f040765913b91c29f
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-21 13:05:31 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - | starts file
+ * Return: - | ends file
+ */
+
+int main(void)
+{
+
+char i;
+
+for (i = 'z'; i <= 'a'; i--)
+{
+
+putchar(i);
+
+}
+
+putchar('\n');
+
+return (0);
+
+}
