9a1b04353e04ab4edb0e797c601060b1ed3bb77d
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-27 13:17:45 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - | starts the file
+ * @a: - | var
+ * @b: - | var
+ * Return: - | ends the file
+ */
+
+int mul(int a, int b)
+{
+
+return (a * b);
+
+}
