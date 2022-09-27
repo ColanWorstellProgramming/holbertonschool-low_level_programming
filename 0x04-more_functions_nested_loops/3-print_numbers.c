20644af85ca3ed5bea9ec73849c3af4650bb6ed5
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-27 13:24:51 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - | starts the file
+ * Return: - | ends the file
+ */
+
+void print_numbers(void)
+{
+
+int i;
+
+for (i = 1; i < 10; i++)
+{
+_putchar(i);
+}
+_putchar("\n");
+}
