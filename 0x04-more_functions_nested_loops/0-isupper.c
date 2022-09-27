e93c99dfd42daf4f6ab57dd5d7df8917812489e4
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-27 13:08:45 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - | starts the file
+ * @c: | var
+ * Return: | ends the file
+ */
+
+int _isupper(int c)
+{
+
+if (c > a..z)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
