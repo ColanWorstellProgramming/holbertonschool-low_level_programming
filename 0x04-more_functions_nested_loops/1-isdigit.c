ca946c54db9e00a422be1345a591c889282abefa
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-27 13:15:38 -0700

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - | starts the file
+ * @c: - | var
+ * Return: - | ends the file
+ */
+
+int _isdigit(int c)
+{
+
+if (c > 47 && c < 58)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
