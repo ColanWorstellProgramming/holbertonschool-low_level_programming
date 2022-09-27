1966cba679c1b996982fc466604141d62c559cc6
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-27 13:34:02 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - | starts the file
+ * Return: - | ends the file
+ */
+
+void print_most_numbers(void)
+{
+
+int i;
+
+for (i = 48; i < 58; i++)
+{
+if (i == 50 || i == 52)
+{
+}
+else
+{
+_putchar(i);
+}
+}
+_putchar('\n');
+}
