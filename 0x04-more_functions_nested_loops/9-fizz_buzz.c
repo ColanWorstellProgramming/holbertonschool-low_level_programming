5767a62b282275feec6d7d0a9533f7b09ebff099
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-27 16:07:35 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,23 +16,22 @@ for (i = 1; i < 101; i++)
 
 if ((i % 15) == 0)
 {
-printf("he");
+printf("FizzBuzz ");
 }
 if ((i % 3) == 0)
 {
-
+printf("Fizz ");
 }
 if ((i % 5) == 0)
 {
-
-
+printf("Buzz ");
 }
 else
 {
-
+printf(i);
 }
 
-
+printf("\n");
 }
 return (0);
 }
