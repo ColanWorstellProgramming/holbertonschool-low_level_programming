daeb7d3bad637e5bbfba0a0761f967a594b73ed2
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-27 15:49:01 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,15 +15,15 @@ for (i = 1; i < 101; i++)
 
 if (i % 15 = 0)
 {
-_putchar("FizzBuzz");
+_putchar('x');
 }
 if (i % 3 = 0)
 {
-_putchar("Fizz");
+_putchar('f');
 }
 if (i % 5 = 0)
 {
-_putchar("Buzz");
+_putchar('b');
 }
 else
 {
