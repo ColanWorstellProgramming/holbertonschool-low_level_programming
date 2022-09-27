d6f920e11ed1172da338c5af57fbe926545c6255
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-27 16:21:33 -0700

    10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_triangle - | starts the file
+ * @size: - | var
+ * Return: - | ends the file
+ */
+
+void print_triangle(int size)
+{
+
+int x, y;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+
+for (x = 1; x <= size; x++)
+{
+for (y = 1; y <= size; y++)
+{
+if (x <= size - a)
+{
+_putchar(' ');
+}
+else
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
