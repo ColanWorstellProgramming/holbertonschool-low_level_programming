3550ed7f5fd6f14147448c39617327cbaeed3dca
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-27 15:45:30 -0700

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - | starts the file
+ * Return: - | ends the file
+ */
+
+int main(void)
+{
+
+int i;
+
+for (i = 0; i < 101; i++)
+{
+
+if (i % 15 = 0)
+{
+printf("FizzBuzz");
+}
+if (i % 3 = 0)
+{
+printf("Fizz");
+}
+if (i % 5 = 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d ", i);
+}
+}
+
+return (0);
+}
