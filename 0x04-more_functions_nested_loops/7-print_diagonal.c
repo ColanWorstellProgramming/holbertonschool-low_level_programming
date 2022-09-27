a6826e1f876f9a487500358441bd5ea423a404b4
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-27 15:10:39 -0700

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - | starts the file
+ * @n: - | var
+ * Return: - | ends the file
+ */
+
+void print_diagonal(int n)
+{
+
+int x = 0;
+
+if (n <= x)
+{
+_putchar('\n');
+}
+else if (n > 0)
+{
+while (x < n)
+{
+_putchar(92);
+x++;
+}
+_putchar('\n');
+}
+}
