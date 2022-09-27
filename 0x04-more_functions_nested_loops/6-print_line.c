4224572ef1bb5b0d8361d137f1660119439dc95b
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-27 15:02:47 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - | starts the file
+ * @n: - | var
+ * Return: - | ends the file
+ */
+
+void print_line(int n)
+{
+
+int x = 0;
+
+if (n == x)
+{
+_putchar('\n');
+}
+else
+{
+while (x < n)
+{
+_putchar('-');
+x++;
+}
+}
+
+}
