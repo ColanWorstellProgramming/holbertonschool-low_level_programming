658d38bd047bdfbf1fc9072e357dafeafb55fbb8
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-27 20:03:23 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - | starts the file
+ * @s: - | var
+ * Return: - | ends the file
+ */
+
+void rev_string(char *s)
+{
+
+int y = 0;
+
+if (s[0] != '\0')
+{
+
+while (s[y] != '\0')
+{
+y++;
+}
+
+while (y >= 0)
+{
+if (s[y] != '\0')
+{
+putchar(s[y]);
+y--;
+}
+else
+{
+}
+}
+
+}
+putchar('\n');
+}
