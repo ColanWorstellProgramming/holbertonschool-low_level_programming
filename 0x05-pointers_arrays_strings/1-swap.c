99e940f437efa685921f49f15e8c18d2577427af
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-27 18:58:46 -0700

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - | starts the file
+ * @a: - | var
+ * @b: - | var
+ * Return: - | ends the file
+ */
+
+void swap_int(int *a, int *b)
+{
+
+int *x = *a;
+*a = *b;
+*b = *x;
+
+}
