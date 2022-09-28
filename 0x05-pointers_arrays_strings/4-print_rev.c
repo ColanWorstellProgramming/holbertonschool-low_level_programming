14a13aa449ee2c396ec3b2bf1885dfd9a3290539
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-27 19:25:09 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - | starts the file
+ * @s: - | var
+ * Return: - | ends the file
+ */
+
+void print_rev(char *s)
+{
+
+int i = 0;
+int y = 0;
+int counter = 0;
+int revcounter = 0;
+char x[] = s[];
+
+while (x[y] != '\0')
+{
+counter++;
+}
+
+for (i = counter; i > 0; i--)
+{
+x[revcounter] = s[counter];
+revcounter++;
+}
+
+while (counter >= 0)
+{
+putchar(x[counter]);
+counter--;
+}
+
+}
