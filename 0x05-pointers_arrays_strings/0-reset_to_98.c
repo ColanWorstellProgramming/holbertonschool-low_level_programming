55382feabb988237170cfe820a61530d0667a0e3
Colan Worstell <Cworstellprogramming@gmail.com>
2022-09-27 18:32:23 -0700

    main

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - | starts the file
+ * @c: - | var
+ * Return: - | ends the file
+ */
+
+int _isupper(int c)
+{
+
+if (c > 64 && c < 91)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
