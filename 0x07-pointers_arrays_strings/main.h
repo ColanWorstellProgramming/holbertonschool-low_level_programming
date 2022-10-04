eebae734cda5f485d919b34ea6552cdaa4f9c30a
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-04 15:51:21 -0700

    README.md

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src, int n);
+char *_strcpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *sc2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
