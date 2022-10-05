d0f941d9c33e04283b2835f333661970aef7f73d
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-04 21:35:17 -0700

    m

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - | starts the file
+ * @a: - | var
+ * Return: - | ends the file
+ */
+
+void print_chessboard(char (*a)[8])
+{
+
+int x, y;
+
+for (x = 0; i < 8; i++)
+{
+for (y = 0; y < 8; y++)
+{
+_putchar(a[x][y]);
+}
+_putchar('\n');
+}
+
+}
