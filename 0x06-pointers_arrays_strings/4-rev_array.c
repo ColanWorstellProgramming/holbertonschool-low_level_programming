e76c47f8c4c374ee1723b4abae2e1ab4f48acd8b
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-04 18:50:05 -0700

    4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,18 +13,11 @@ void reverse_array(int *a, int n)
 int i;
 int y = n - 1;
 int x;
-int co = 0;
 
 for (i = 0; i < y; i++)
 {
 x[i] = a[y];
 y--;
-}
-
-while (co < (n-1))
-{
-a[co] = x[co];
-co++;
 }
 
 }
