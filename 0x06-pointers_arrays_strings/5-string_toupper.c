a22f7d11997f4c2e855d9e75bd04128b1adb01f6
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-04 19:00:36 -0700

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - | starts the file
+ * @*: - | var
+ * Return: - | ends the file
+ */
+
+char *string_toupper(char *)
+{
+
+int i = 0;
+
+while (*[i] != '\0')
+{
+if (*[i] > 96 && *[i] < 123)
+{
+*[i] = *[i] - 32;
+}
+i++;
+}
+
+}
