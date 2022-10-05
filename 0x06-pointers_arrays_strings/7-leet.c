fd1260a73f90975cfcf8295841bd33f1837e6ad1
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-04 20:11:47 -0700

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * leet - | starts the file
+ * @s: - | var
+ * Return: - | ends the file
+ */
+
+char *leet(char *s)
+{
+
+char x[] = "aAeEoOtTlL";
+char y[] = "4455007711";
+
+int i = 0;
+int z;
+
+while (s[i])
+{
+
+for (z = 0; z < 10; z++)
+{
+
+if (s[i] = x[z])
+{
+s[i] = y[z];
+}	
+
+i++;
+}
+}
+
+return (s);
+}
