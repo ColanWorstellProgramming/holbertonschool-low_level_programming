8e57b2ec34931fb59bc8fa069e1dd3ec558c2be1
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-04 17:52:25 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,13 +10,13 @@
 int _strcmp(char *s1, char *s2)
 {
 
-if (*s1 < *s2)
+if (s1 < s2)
 {
-return (*s1 - *s2);
+return (s1 - s2);
 }
-else if (*s1 > *s2)
+else if (s1 > s2)
 {
-return (*s2 - *s1);
+return (s2 - s1);
 }
 else
 {
