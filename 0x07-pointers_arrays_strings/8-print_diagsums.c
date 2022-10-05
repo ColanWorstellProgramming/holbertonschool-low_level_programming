5bfe8a86e0052f0058f78fcd761d2277e4af9e95
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-04 21:42:36 -0700

    8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - | starts the file
+ * @a: - | var
+ * @size: - | var
+ * Return: - | ends the file
+ */
+
+void print_diagsums(int *a, int size)
+{
+
+int i;
+int x = 0;
+int y = 0;
+
+for (i = 0; x < size; x++)
+{
+x = x + a[(size + 1) * i];
+y = y + a[(size - 1) * (i + 1)];
+}
+
+printf("%d, %d\n", x, y);
+
+}
