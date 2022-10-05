aecd99a9a87ce142dbe9e40993a2e5b0f381496c
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-04 17:32:46 -0700

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * *_strcmp - | starts the file
+ * @s1: - | var
+ * @s2: - | var
+ * Return: - | ends the file
+ */
+
+char *_strcmp(char *s1, char *s2)
+{
+
+if (s1 < s2)
+return (s1-s2)
+
+else if (s1 > s2)
+return (s2-s1)
+
+else if (s1 == s2)
+return (0);
+
+}
