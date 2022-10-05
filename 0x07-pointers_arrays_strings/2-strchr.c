d226aa79d2ad5b2ebbde8e7d2d0ba9c45faeac96
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-04 20:35:11 -0700

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - | starts the file
+ * @s: - | var
+ * @c: - | var
+ * Return: - | ends the file
+ */
+
+char *_strchr(char *s, char c)
+{
+
+int i = 0;
+
+while (s != '\0')
+{
+if (s[i] == c)
+{
+return (s[i]);
+}
+i++;
+}
+
+return (NULL);
+}
