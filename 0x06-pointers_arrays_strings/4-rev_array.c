026c2adfc1e790985327dd3d787d64ca6b5e8414
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-04 18:31:42 -0700

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - | starts the file
+ * @a: - | var
+ * @n: - | var
+ * Return: - | ends the file
+ */
+
+void reverse_array(int *a, int n)
+{
+
+int y;
+int ar;
+int i = n;
+int x = 0;
+
+while (i >= 0)
+{
+ar[x] = a[i]
+x++;
+i--;
+}
+
+for (y = 0; y < n; y++)
+{
+a[y] = x[y];
+}
+
+}
