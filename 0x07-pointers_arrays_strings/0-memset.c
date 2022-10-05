e943e99397a11a522606607983130099e4128214
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-04 20:22:39 -0700

    0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _memset - | starts the file
+ * @s: - | var
+ * @b: - | var
+ * @n: - | var
+ * Return: - | ends the file
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+
+
+return (s);
+}
