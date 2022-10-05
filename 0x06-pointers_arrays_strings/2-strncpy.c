bb37d923b5a11ea73bcc673b74e3f83bb5c49443
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-04 17:08:28 -0700

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * *_strncpy - | starts the file
+ * @dest: - | var
+ * @src: - | var
+ * @n: - | var
+ * Return: - | ends the file
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+int i = 0;
+int y = 0;
+
+while (src[i] != '\0' && i < n)
+{
+dest[i] = src[i];
+i++;
+}
+
+while (src[y] != '\0')
+{
+y++;
+}
+
+if (y < n)
+{
+dest[x + i] = '\0';
+}
+
+return (dest);
+}
