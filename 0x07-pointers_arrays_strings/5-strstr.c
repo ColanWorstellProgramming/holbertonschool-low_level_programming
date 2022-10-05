b54e61904f128b8b07660b4382f8ae98c688ff08
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-04 21:28:55 -0700

    5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strstr - | starts the file
+ * @haystack: - | var
+ * @needle: - | var
+ * Return: - | ends the file
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+unsigned int x = 0;
+unsigned int y = 0;
+
+while (haystack[i])
+{
+
+while (needle[y] && haystack[i] == needle[0])
+{
+
+if (haystack[x + y] == needle[0])
+{
+y++;
+}
+else
+{
+break;
+}
+}
+if (needle[y])
+{
+x++;
+y = 0;
+}
+else
+{
+return (haystack + i);
+}
+}
+return (0);
+}
