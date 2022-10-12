c978560362974344b295472e53adc038600a1147
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-11 17:42:47 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - | starts the file
+ * @n: - | var
+ * Return: - |
+ */
+
+int _sqrt_recursion(int n)
+{
+
+if (n <= 0)
+{
+return (-1);
+}
+else
+{
+return (_sqrt(n, 0);
+}
+
+}
+
+/**
+ *  * _sqrt - | starts the file
+ *   * @x: - | var
+ *    * @y: - | var
+ *     * Return: - |
+ *      */
+
+int _sqrt(int x, int y)
+{
+
+if (x == ( y * y))
+{
+return (x);
+}
+else if (x <= (y * y))
+{
+return (-1);
+}
+else
+{
+return (_sqrt(x, y + 1));
+}
+
+}
