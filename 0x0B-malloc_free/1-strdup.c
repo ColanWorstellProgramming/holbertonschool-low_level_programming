3aaae660dae6f1d2d3967b971c181549567eb866
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-11 19:01:52 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,46 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - | starts the file
+ * @str: - | var
+ * Return: - |
+ */
+
+char *_strdup(char *str)
+{
+char *y;
+int i;
+
+if (str == NULL)
+return (NULL);
+
+y = malloc(_strlen(str) + 1);
+if (y == NULL)
+return (NULL);
+for (i = 0; i < _strlen(str); i++)
+{
+y[i] = str[i];
+}
+return (y);
+}
+
+/**
+ *  * _strlen - | starts the file
+ *   * @s: - | var
+ *    * Return: - |
+ *     */
+
+int _strlen(char *s)
+{
+int len = 0;
+
+while (*s)
+{
+s++;
+len++;
+}
+
+return (len);
+}
