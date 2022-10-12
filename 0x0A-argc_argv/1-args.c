16e02a4fc934a65991cfd548d02c0eb411a4f3d1
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-11 18:13:12 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - | starts the file
+ * @argc: - | var
+ * @argv: - | var
+ * Return: - |
+ */
+
+int main(int argc, char *argv[])
+{
+
+int i;
+
+for (i = 0; argv[i] != '\0'; i++)
+{
+
+}
+printf(i);
+return (0);
+}
