e058e3cfe257743aec976ccc03d2f7b33387825e
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-11 18:55:58 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,36 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - | starts the file
+ * @size: - | var
+ * @c: - | var
+ * Return: - |
+ */
+
+char *create_array(unsigned int size, char c)
+{
+
+char *y;
+unsigned int i;
+
+if (size == 0)
+{
+return (NULL);
+}
+else
+{
+y = malloc(size * 1);
+if (y == NULL)
+{
+return (NULL);
+}
+else
+for (i = 0; i < size; i++)
+{
+y[i] = c;
+}
+}
+return y;
+}
