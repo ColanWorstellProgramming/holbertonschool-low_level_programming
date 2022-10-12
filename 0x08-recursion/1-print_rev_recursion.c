091c680d8ba2ae331dac68cb3aa1aee3ffa7991d
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-11 17:19:15 -0700

    1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - | starts the file
+ * @s: - | var
+ */
+
+void _print_rev_recursion(char *s)
+{
+
+if (*s != '\0')
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+
+}
