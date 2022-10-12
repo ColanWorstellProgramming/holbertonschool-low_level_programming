08bec8cc8149b735d69cbf24f2b59d4748d1537c
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-11 18:30:05 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,15 +10,15 @@
 int main(int argc, char *argv[])
 {
 
-int f = 1;
 int i;
+int f = 1;
 
 if (argc == 3)
 {
 
 for (i = 1; i < argc; i++)
 {
-f *= atoi(argv[i]);
+f = f * atoi(argv[i]);
 }
 
 printf("%d\n", f);
