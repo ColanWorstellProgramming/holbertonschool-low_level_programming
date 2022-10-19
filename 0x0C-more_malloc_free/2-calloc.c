aeebcfdf4e77e7c0d938a1431fa84a8b8e986c88
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-18 18:04:23 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *_calloc - | starts the file
+ * @nmemb: - | var
+ * @size: - | var
+ * Return: | ends the file
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+char *mem;
+unsigned int x;
+
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
+mem = malloc(nmemb * size);
+if (mem == NULL)
+{
+return (NULL);
+}
+for (x = 0; x < nmemb * size; i++)
+{
+mem[x] = 0;
+}
+return (mem);
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *array_range - | starts the file
+ * @min: - | var
+ * @max: - | var
+ * Return: | ends the file
+ */
+
+int *array_range(int min, int max)
+{
+
+
+
+}
