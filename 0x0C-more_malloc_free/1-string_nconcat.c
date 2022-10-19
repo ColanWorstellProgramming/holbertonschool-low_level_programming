6491e33055c7735d928cc2777521f4b150389ab0
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-18 17:43:57 -0700

    1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - | starts the file
+ * @s1: - | var
+ * @s2: - | var
+ * @n: - | var
+ * Return: | ends the file
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+int x, y;
+char *mem, *c;
+
+if (s2 == NULL)
+{
+s2 = "";
+}
+
+if (s1 == NULL)
+{
+s1 = "";
+}
+
+if (n >= strlen(s2))
+{
+n = strlen(s2);
+}
+
+mem = malloc(strlen(s1) + n + 1);
+
+if (mem == NULL)
+{
+return (NULL);
+}
+
+for (x = 0; x < strlen(s1); x++)
+{
+mem[i] = s1[i];
+}
+
+for (y = 0; y < n; y++)
+{
+mem[x + y] = s2[y];
+}
+
+mem[x + y] = '\0';
+
+c = malloc(5);
+free(c);
+
+return (mem);
+
+}
