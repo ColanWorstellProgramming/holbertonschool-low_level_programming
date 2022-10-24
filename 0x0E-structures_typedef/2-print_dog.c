e55c1d9badb4eb64d113942b592b5225a24fd4c2
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-24 14:14:56 -0700

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - starts the file
+ * @d: var
+ */
+
+void print_dog(struct dog *d)
+{
+
+	if (d != NULL)
+	{
+
+	if (d.name == NULL)
+	{
+	
+		printf("Name: (nil)");
+
+	}
+	else
+	{
+	
+		printf("Age: %d\n", d.name);
+
+	}
+
+	if (d.age == NULL)
+	{
+	
+		printf("Age: (nil)");
+
+	}
+	else
+	{
+	
+		printf("Age: %d\n", d.age);
+
+	}
+	if (d.owner == NULL)
+	{
+	
+		printf("Owner: (nil)");
+
+	}
+	else
+	{
+	
+		printf("Owner: %d\n", d.owner);
+
+	}
+	}
+
+}
