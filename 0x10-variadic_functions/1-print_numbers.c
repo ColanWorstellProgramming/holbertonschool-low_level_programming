5e430db4e2c4189a0056be6b23bba3c3991f058f
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-25 00:51:05 -0700

    test3

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -25,7 +25,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 	for (i = 0; i < (n - 1); i++)
 	{
-	printf(%d%s, va_arg(num, unsigned int), separator);
+	printf("%d%s", va_arg(num, unsigned int), separator);
 	}
 	printf("%d", va_arg(num, unsigned int));
 	}
