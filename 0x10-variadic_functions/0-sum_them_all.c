2e7e95a9861f65bb575dc7e7e8fee4353f314062
Colan Worstell <Cworstellprogramming@gmail.com>
2022-10-25 00:18:09 -0700

    0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+/**
+ * sum_them_all - main
+ * @n: var
+ *
+ * Return: returns
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int x = 0;
+	unsigned int i;
+	va_list num;
+
+	if (n == 0)
+	{
+	return (0);
+	}
+
+	va_start(num, n);
+	for (i = 0; i < n; i++)
+	{
+	x = x + va_arg(num, int);
+	}
+
+	va_end(num);
+
+	return (x);
+}
