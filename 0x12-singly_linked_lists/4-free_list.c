bee2896f1b936daec622d01075c9187e8d6c43af
Colan Worstell <Cworstellprogramming@gmail.com>
2022-11-08 19:22:43 -0800

    test

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - function
+ * @head: list var
+ * Return: n/a
+ */
+void free_list(list_t *head)
+{
+list_t *final;
+
+while(head)
+{
+final = head->next;
+free(head->str);
+free(head);
+head = final;
+}
+}
