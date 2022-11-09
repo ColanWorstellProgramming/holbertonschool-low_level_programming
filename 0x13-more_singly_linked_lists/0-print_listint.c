34066f8cfd2aef5697dc8422cb4019545368b425
Colan Worstell <Cworstellprogramming@gmail.com>
2022-11-08 20:21:16 -0800

    newproject

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - function
+ * @h: list var
+ * Return: node count
+ */
+
+size_t print_listint(const list_t *h)
+{
+size_t num = 0;
+
+while (h)
+{
+	num++;
+	printf("%d\n", h->n);
+	h = h->next;
+}
+
+return (num);
+}
