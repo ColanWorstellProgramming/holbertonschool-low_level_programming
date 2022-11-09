0405314aa096a53be4a2db6bf9467149cd153ab0
Colan Worstell <Cworstellprogramming@gmail.com>
2022-11-08 18:57:15 -0800

    test1

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * add_node - function
+ * @head: list var
+ * @str: str var
+ * Return: address or null
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+char copy;
+list_t final;
+if (str == NULL)
+{
+return (NULL);
+}
+copy = strdup(str);
+final->str = copy;
+}
