bbcfe8bcb06a83653727e2b5474ff5d4032e6948
Colan Worstell <Cworstellprogramming@gmail.com>
2022-11-08 20:45:11 -0800

    error

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,18 +1,19 @@
 #include "lists.h"
 
 /**
- * listint_len - function
+ * print_listint - function
  * @h: list var
  * Return: node count
  */
 
-size_t listint_len(const listint_t *h)
+size_t print_listint(const listint_t *h)
 {
 size_t num = 0;
 
 while (h)
 {
 	num++;
+	printf("%d\n", h->n);
 	h = h->next;
 }
 
