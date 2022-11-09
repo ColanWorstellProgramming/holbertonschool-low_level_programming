a799ecb65c455bf16eb08846455e69475ef0e9f6
Colan Worstell <Cworstellprogramming@gmail.com>
2022-11-08 21:52:11 -0800

    4

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,7 +7,7 @@
  * Return: value
  */
 
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 int o, w, len = 0;
 
@@ -24,7 +24,7 @@ if (text_content != NULL)
 	}
 }
 
-o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+o = open(filename, O_WRONLY | O_APPEND);
 w = write(o, text_content, len);
 
 if (o == -1 || w == -1)
