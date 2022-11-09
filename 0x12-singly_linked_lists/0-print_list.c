0c4a1fe28b3a5aa5ce9b006ba6fd8175d7a6ee06
Colan Worstell <Cworstellprogramming@gmail.com>
2022-11-08 18:42:49 -0800

    test

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,16 +11,17 @@ size_t print_list(const list_t *h)
 size_t num = 0;
 while (h)
 {
-if (h->str == NULL)
-{
-printf("[0] (nil)\n");
-}
-else
+if (h->str != NULL)
 {
 printf("[%d] %s\n", h->len, h->str);
 num++;
 h = h->next;
 }
+else
+{
+printf("[0] (nil)\n");
+}
+
 }
 return (num);
 }
