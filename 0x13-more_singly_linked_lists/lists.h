5cd91a6b977ebfe934cdb8a5761c29b11527804a
Colan Worstell <Cworstellprogramming@gmail.com>
2022-11-08 20:41:30 -0800

    listissue

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -9,13 +9,13 @@
 #include <stdlib.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
+ * struct listint_s - singly linked list
+ * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
  */
+
 typedef struct listint_s
 {
 	int n;
