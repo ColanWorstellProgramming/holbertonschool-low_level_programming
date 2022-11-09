e9cfaf2d4f1d7c313fabe44e06b9d8810276f9bb
Colan Worstell <Cworstellprogramming@gmail.com>
2022-11-08 20:11:50 -0800

    bitproblems

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * clear_bit - function
+ * @n: var
+ * @index: var
+ * Return: int
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int xor = n ^ m, bits = 0;
+
+while (xor > 0)
+{
+	bits += (xor & 1);
+	xor >>= 1;
+}
+
+return (bits);
+}
