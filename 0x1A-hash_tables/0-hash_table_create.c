ce1bf7858bdd159847ce287938a78cfc7a96f472
Colan Worstell <Cworstellprogramming@gmail.com>
2022-11-15 20:37:12 -0800

    betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,6 +13,13 @@ hash_table_t *ht;
 hash_node_t **hn;
 unsigned long int i;
 
+if (size == 0)
+{
+
+return (NULL);
+
+}
+
 ht = malloc(sizeof(hash_table_t));
 hn = malloc(sizeof(hash_node_t *) * size);
 
