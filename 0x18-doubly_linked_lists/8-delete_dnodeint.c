9331cc4fbf4afa3e8c46dd10753ed6c026dbc2b1
Colan Worstell <Cworstellprogramming@gmail.com>
2022-11-15 19:34:21 -0800

    betty

--- 0x18-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x18-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,13 +9,10 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-
 dlistint_t *temp = *head, *temp1, *temp2;
 unsigned int counter = 0;
-
 while (temp && counter < index)
 {
-
 if (temp == NULL)
 {
 return (-1);
@@ -26,7 +23,6 @@ counter++;
 temp = temp->next;
 }
 }
-
 if (temp == NULL)
 {
 return (-1);
@@ -35,25 +31,19 @@ if (*head == temp)
 {
 *head = temp->next;
 }
-
 if (temp->next != NULL)
 {
 temp->next->prev = temp->prev;
 }
-
 if (temp->prev != NULL)
 {
 temp->prev->next = temp->next;
 }
-
 temp1 = temp->prev;
 temp2 = temp->next;
-
 temp->prev = temp1;
 temp->next = temp2;
-
 free(temp);
-
 return (1);
 
 }
