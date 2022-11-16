bf1565fec36c27e92a793dddabb70d261b102314
Colan Worstell <Cworstellprogramming@gmail.com>
2022-11-15 16:51:08 -0800

    1

---
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - main function
+ * @h: var
+ * Return: size_t
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+
+int count = 0;
+
+while (h)
+{
+printf("%d\n", h->n);
+count++;
+}
+
+return (count);
+}
