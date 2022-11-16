8b3765f52965bba154c77667570e4b5986cdf604
Colan Worstell <Cworstellprogramming@gmail.com>
2022-11-15 19:01:44 -0800

    list

--- 0x18-doubly_linked_lists/9-print_dlistint_backward.c
+++ 0x18-doubly_linked_lists/9-print_dlistint_backward.c
@@ -10,26 +10,22 @@ size_t print_dlistint_backward(const dlistint_t *h)
 {
 
 size_t count = 0;
-size_t max;
 
-while (h)
+if (!h)
 {
-count++;
-if (h->next != NULL)
+return (0);
+}
+
+while (h->next != NULL)
 {
 h = h->next;
+count++;
 }
-}
-max = count;
 while (count > 0)
 {
 printf("%d\n", h->n);
-count--;
-if (h->prev != NULL)
-{
 h = h->prev;
 }
-}
 
-return (max);
+return (count);
 }
