36d3b266f158ed922fa6b0c2ff322ff6a7a26e26
Colan Worstell <Cworstellprogramming@gmail.com>
2022-11-15 17:48:31 -0800

    3?

---
+++ 0x18-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - main function
+ * @head: var
+ * @n: var
+ * Return: dlistint_t
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+
+dlistint_t *new = malloc(sizeof(dlistint_t));
+dlistint_t *temp = *head;
+
+if (new == NULL)
+{
+return (NULL);
+}
+
+while(temp->next != NULL)
+{
+temp = temp->next;
+
+if (temp->next == NULL)
+{
+new->n = n;
+new->prev = temp->prev;
+new->next = NULL;
+temp->next = new;
+}
+}
+
+*head = temp;
+
+return (new);
+}
