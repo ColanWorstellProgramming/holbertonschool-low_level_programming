89663fba09a2c6f1e3007cf359c762bafacb08bf
Colan Worstell <cworstellprogramming@gmail.com>
2023-08-12 23:49:22 -0500

    binary

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - function
+ * @array: var
+ * @size: var
+ * @value: var
+ *
+ * Return: value
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+    size_t left = 0;
+    size_t right = size - 1;
+	size_t i = 0;
+    int middle;
+
+    while (left <= right)
+    {
+        middle = left + (right - left) / 2;
+
+        printf("Searching in array: ");
+        for (i = left; i <= right; i++)
+        {
+            printf("%d", array[i]);
+            if (i < right)
+                printf(", ");
+        }
+        printf("\n");
+
+        if (array[middle] == value)
+            return middle;
+
+        if (array[middle] < value)
+            left = middle + 1;
+        else
+            right = middle - 1;
+    }
+
+    return -1;
+}
