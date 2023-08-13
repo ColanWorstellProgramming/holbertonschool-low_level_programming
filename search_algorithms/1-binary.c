5221f5306904c36f8dd8eb2c8759cf4b09064e2e
Colan Worstell <cworstellprogramming@gmail.com>
2023-08-12 23:50:30 -0500

    binary

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -16,6 +16,11 @@ int binary_search(int *array, size_t size, int value)
 	size_t i = 0;
     int middle;
 
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
     while (left <= right)
     {
         middle = left + (right - left) / 2;
